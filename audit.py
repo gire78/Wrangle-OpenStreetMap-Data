24565e24ecb12510be39e4985b30a76d663a9863
gire78 <79297406+gire78@users.noreply.github.com>
2021-02-20 19:04:45 -0500

    Update audit.py

--- audit.py
+++ audit.py
@@ -1,155 +1,97 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""
+Your task in this exercise has two steps:
 
-''' File including the auditing functions to be called through audit_v at the time of transforming the Map-XML to CSV'''
-import codecs
-###Audit the Phone Numbers####
+- audit the OSMFILE and change the variable 'mapping' to reflect the changes needed to fix 
+    the unexpected street types to the appropriate ones in the expected list.
+    You have to add mappings only for the actual problems you find in this OSMFILE,
+    not a generalized solution, since that may and will depend on the particular area you are auditing.
+- write the update_name function, to actually fix the street name.
+    The function takes a string with street name as an argument and should return the fixed name
+    We have provided a simple test so that you see what exactly is expected
+"""
+import xml.etree.cElementTree as ET
+from collections import defaultdict
+import re
+import pprint
 
-def delete_spec_chars(number):
-    '''Delete special characters and spaces'''
+OSMFILE = "sample.osm"
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 
-    char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c ' 
-    return filter(lambda x: not (x in char), number)
 
-def insert_spaces(number):
-    '''Insert spaces acc to the format'''
-    number = number[:3] + ' ' + number[3:5] + ' ' + number[5:]
-    return number
-    
-def get_raw_local(number):
-    '''Clean the number and get raw local num'''
+expected = ["Road", "Street", "Drive", "Highway", "Court", "Lane", "Avenue", "Circle", "Place", "Mountain", 
+            "Square", "Terrace", "North", "East", "Alley", "West", "Lake"]
 
-    #get the local number
-    number = number.lstrip('+')
-    number = number.lstrip('00')
-    number = number.lstrip('48')
+# UPDATE THIS VARIABLE
+mapping = { "St": "Street",
+            "street": "Street",
+            "Rd" : "Road",
+            "road" : "Road",
+            "Dr" : "Drive",
+            "Hwy" : "Highway",
+            "Ct" : "Court",
+            "Ln" : "Lane",
+            "Ave" : "Avenue",
+            "Cir" : "Circle",
+            "Pl" : "Place",
+            "Mt" : "Mountain",
+            "Sq" : "Square",
+            "Ter": "Terrace",
+            "N": "North",
+            "E" : "East",
+            "alley" : "Alley",
+            "W" : "West",
+            "lake": "Lake"
+          }
 
-    # delete the second number or extra digits over 9
-    if len(number)>=9:
-        number = number[:9]
-    return number
-        
-def is_foreign(number):
-    '''Check if foreign number'''
-    
-    #Check if foreign
-    if number.startswith('00') or number.startswith('+'):
-        number = number.lstrip('+')
-        number = number.lstrip('00')
-        if not (number.startswith('48') or number.startswith('22')):
-            return True
- 
-def correct_phone(number):
+def audit_street_type(street_types, street_name):
+    m = street_type_re.search(street_name)
+    if m:
+        street_type = m.group()
+        if street_type not in expected:
+            street_types[street_type].add(street_name)
 
-    #Delete special characters and spaces
-    number = delete_spec_chars(number)
 
-    if not is_foreign(number):
+def is_street_name(elem):
+    return (elem.attrib['k'] == "addr:street")
 
-        #get the local number
-        number = get_raw_local(number)
-        
-        #add the country prefix
-        number = ('+48'+ number)
 
-    number = insert_spaces(number)   
-    return number
+def audit(osmfile):
+    osm_file = open(osmfile, "r")
+    street_types = defaultdict(set)
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
 
-####Audit the key:Building#####
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_street_name(tag):
+                    audit_street_type(street_types, tag.attrib['v'])
+    osm_file.close()
+    return street_types
 
-#Set the global variable for the building mapping
-building_mapping={}
 
-def correct_building(val):
+def update_name(name, mapping):
 
-    if  'shop' in val or 'store' in val or 'retail' in val or 'food' in val   or 'fuel' in val:
-        building_mapping[val] = 'retail'
-    elif 'office' in val or 'commercial' in val:
-        building_mapping[val] = 'commercial'
-    elif 'residential' in val:
-        building_mapping[val] = 'apartments'
-    elif  'enter' in val or 'hall' in val or 'library' in val or 'palace'in val or 'monument' in val or 'castle' in val or 'civ' in val:
-        building_mapping[val] = 'civic'
-    elif  'clini' in val or 'docto' in val:
-        building_mapping[val] = 'hospital'
-    elif 'muse' in val:
-        building_mapping[val] = 'museum'
-    elif 'ruin' in val or 'collapsed' in val:
-        building_mapping[val] = 'ruins'
-    elif 'terrac' in val:
-        building_mapping[val] = 'terrace'    
-    elif 'serv' in val or 'power' in val:
-        building_mapping[val] = 'service'  
-    elif 'avia' in val:
-        building_mapping[val] = 'hangar' 
-    elif 'mbass' in val:
-        building_mapping[val] = 'embassy'
-    elif 'boat' in val:
-        building_mapping[val] = 'houseboat'
-    elif 'otel' in val:
-        building_mapping[val] = 'hotel'
-    elif 'detache' in val:
-        building_mapping[val] = 'detached'  
-    elif  'convent' in val or 'basilica' in val or 'monastery' in val :
-        building_mapping[val] = 'church'
-    elif  'factory' in val or 'storage_tank' in val or 'silo' in val :
-        building_mapping[val] = 'industrial'
-    elif  'shrine' in val :
-        building_mapping[val] = 'shrine'
-    elif  'glass' in val :
-        building_mapping[val] = 'conservatory'
+    for key,value in mapping.items():
+        if key in name:
+            return name.replace(key,value)
 
-    else: 
-        building_mapping[val]=val
+    return name
 
-    return building_mapping[val]
 
-####Audit the name:Cafe for key:'amenity'#####
+def test():
+    st_types = audit(OSMFILE)
+    assert len(st_types) == 3
+    pprint.pprint(dict(st_types))
 
-#Set the global variable for the building mapping
-cafe_names_mapping={}
+  """  for st_type, ways in st_types.iteritems():
+        for name in ways:
+            better_name = update_name(name, mapping)
+            print name, "=>", better_name
+            if name == "West Lexington St.":
+                assert better_name == "West Lexington Street"
+            if name == "Baldwin Rd.":
+                assert better_name == "Baldwin Road"
+                """
 
-def correct_cafe_names(val):    
-    if  'green' in val.lower() or 'nero' in val.lower() :
-        cafe_names_mapping[val] = 'Green Caffè Nero'
-    elif 'costa' in val.lower():
-        cafe_names_mapping[val] = 'Costa Coffee'
-    elif 'blikle' in val.lower():
-        cafe_names_mapping[val] = 'A. Blikle'
 
-    else: 
-        cafe_names_mapping[val]=val
-
-    return cafe_names_mapping[val]
-
-def audit_v(is_cafe, tag):
-    '''Audit v attribute and return value'''
-
-    v = tag.attrib['v'].encode('utf-8')
-
-    #map building
-    if tag.attrib['k']=='building':
-        if v not in building_mapping.keys():
-            cb = correct_building(v)
-            print('Mapped: ' + v + ' => ' + cb)
-        else: cb = building_mapping[v]
-        return cb
-
-    #map Cafe Names
-    elif is_cafe:
-        if tag.attrib['k']=='name':
-            if v not in cafe_names_mapping.keys():
-                cn = correct_cafe_names(v) 
-                print('Mapped: ' + v + ' => ' + cn)
-                return cn
-            else: return cafe_names_mapping[v]
-
-        
-    # map phone
-    elif tag.attrib['k']=='phone':  
-        cf = correct_phone(v)      
-        print('Mapped: ' + v + ' => ' + cf)
-        return cf
-
-    return v
-
+if __name__ == '__main__':
+    test()
